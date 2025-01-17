882f9fe1dd20e913a6cbfa4f0e5eb86ca76d1783
zainali28 <zainali123113@gmail.com>
2025-01-17 05:25:32 +0500

    added favourites caching

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,9 +1,36 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  const saveFavourites = async () => {
+    try {
+      const jsonValue = JSON.stringify(favourites);
+      await AsyncStorage.setItem("favs", jsonValue);
+    } catch (e) {
+      console.log("error storing", e);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("favs");
+      if (jsonValue !== null) setFavourites(JSON.parse(jsonValue));
+    } catch (e) {
+      console.log("error loading", e);
+    }
+  };
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites();
+  }, [favourites]);
 
   const add = (restaurant) => {
     setFavourites([...favourites, restaurant]);
